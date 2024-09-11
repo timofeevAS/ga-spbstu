72d5e4ad19c220ca884413f42298778b1b7fe169
Alexandr <a.timoff3@gmail.com>
2024-09-11 22:06:30 +0300

    add basic class for population

---
+++ core/population.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import List
+import random
+
+from core.individual import Individual
+
+
+class Population(ABC):
+    def __init__(self, population_size: int):
+        self.population_size = population_size
+        self.individuals: List[Individual] = []
+
+    @abstractmethod
+    def init_population(self) -> None:
+        pass
+
+    @abstractmethod
+    def selection(self) -> List[Individual]:\
+        pass
+
+    @abstractmethod
+    def fitness_function(self, individual: Individual) -> float:
+        pass
