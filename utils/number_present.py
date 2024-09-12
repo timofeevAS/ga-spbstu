9d0f6af3c51a8bf856f74fba3b4d722e75ee3347
timofeevAS <a.timoff3@gmail.com>
2024-09-12 11:59:50 +0300

    change type hint to float for parametrs, which represent range limit.

--- utils/number_present.py
+++ utils/number_present.py
@@ -1,15 +1,17 @@
 import math
 
-def auto_precision(a: int, b: int, precision=0):
+def auto_precision(a: float, b: float, precision=0):
     length = (b - a + 1) * (10 ** precision)
     print(range)
     res = math.ceil(math.log2(length))
     return res
 
-def binary_arithmetic(code: str, a: int, b: int, precision=0) -> float:
+def binary_arithmetic_number(code: str, a: float, b: float, precision=0) -> float:
     n = auto_precision(a, b, precision)
 
     digit = int(code, 2)
     result = a + digit * ((b - a) / (2 ** n - 1))
     return result
 
+def from_int_to_binary_string(digit: int):
+    return bin(digit)[2:]
