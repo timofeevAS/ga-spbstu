cc64f558cbd297311a8afacac9a13f84f93388d9
timofeevAS <a.timoff3@gmail.com>
2024-09-12 23:34:17 +0300

    add abstract methods:  - selection -> reproduction -> mutation -> reduction  to impl genetic algorithm

--- core/ga.py
+++ core/ga.py
@@ -4,9 +4,10 @@ from core.population import Population
 
 
 class GeneticAlgorithm(ABC):
-    def __init__(self, population: Population):
-        self.population = population
-        population.init_population()
+    def __init__(self, population_size: int, crossover_p: float, mutation_p: float):
+        self.population_size = population_size
+        self.crossover_p = crossover_p
+        self.mutation_p = mutation_p
 
     @abstractmethod
     def round(self) -> None:
@@ -16,8 +17,40 @@ class GeneticAlgorithm(ABC):
         """
         pass
 
+    @abstractmethod
+    def selection(self) -> None:
+        """
+        Method to selection step of genetic algorithm.
+        :return:
+        """
+
+    @abstractmethod
     def reproduction(self) -> None:
         """
         Method to reproduction step of genetic algorithm.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def mutation(self) -> None:
+        """
+        Method to mutation step of genetic algorithm.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def reduction(self) -> None:
+        """
+        Method to reduction step of genetic algorithm.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def run(self) -> None:
+        """
+        Method to run genetic algorithm.
         :return:
         """
