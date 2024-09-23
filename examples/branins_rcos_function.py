4baebf41adf1a8f5738328c23d04dead88ab45b9
timofeevAS <a.timoff3@gmail.com>
2024-09-23 11:01:15 +0300

    add example of showing plot of branins_rcos_function

--- examples/branins_rcos_function.py
+++ examples/branins_rcos_function.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 from core.function import TwoParamFunction
 
@@ -15,3 +17,25 @@ class BraninsRcosFunction(TwoParamFunction):
         param1_name='x1'
         param2_name='x2'
         super().__init__(function_expr, param1_name, param2_name)
+
+if __name__ == '__main__':
+    # Example of plot:
+    num: int = 50
+    x1 = np.linspace(-5, 10, num, dtype=float)
+    x2 = np.linspace(0, 15, num, dtype=float)
+
+    X1, X2 = np.meshgrid(x1, x2)
+
+    f = BraninsRcosFunction()
+    Z = np.array([[f.evaluate(X1[i][j], X2[i][j]) for j in range(num)] for i in range(num)])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.plot_surface(X1, X2, Z, cmap='hsv')
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('f(X1, X2)')
+    ax.set_title('Branin RCos Function')
+
+    plt.show()
