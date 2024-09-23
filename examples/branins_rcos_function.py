2d02d9c251abdf6e7f3d8691e6c539e9886f579c
timofeevAS <a.timoff3@gmail.com>
2024-09-23 14:31:42 +0300

    add showing function for BraninsRcosFunction()

--- examples/branins_rcos_function.py
+++ examples/branins_rcos_function.py
@@ -18,24 +18,36 @@ class BraninsRcosFunction(TwoParamFunction):
         param2_name='x2'
         super().__init__(function_expr, param1_name, param2_name)
 
+    def print_plot_with_points(self, dots):
+        # Example of plot:
+        num: int = 50
+        x1 = np.linspace(-5, 10, num, dtype=float)
+        x2 = np.linspace(0, 15, num, dtype=float)
+
+        X1, X2 = np.meshgrid(x1, x2)
+
+        f = BraninsRcosFunction()
+        Z = np.array([[f.evaluate(X1[i][j], X2[i][j]) for j in range(num)] for i in range(num)])
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.plot_surface(X1, X2, Z, cmap='hsv', alpha=0.5)
+        ax.set_xlabel('X1')
+        ax.set_ylabel('X2')
+        ax.set_zlabel('f(X1, X2)')
+        ax.set_title('Branin RCos Function')
+
+        # Plot dots on the surface
+        print(dots)
+        if dots:
+            dots_x1, dots_x2 = zip(*dots)
+            dots_z = [f.evaluate(dot_x1, dot_x2) for dot_x1, dot_x2 in dots]
+
+            dots_z = list(map(lambda x: x + 5, dots_z))
+            ax.scatter(dots_x1, dots_x2, dots_z, color='black', s=50, label='Points', marker='P')
+
+        plt.show()
+
 if __name__ == '__main__':
-    # Example of plot:
-    num: int = 50
-    x1 = np.linspace(-5, 10, num, dtype=float)
-    x2 = np.linspace(0, 15, num, dtype=float)
-
-    X1, X2 = np.meshgrid(x1, x2)
-
-    f = BraninsRcosFunction()
-    Z = np.array([[f.evaluate(X1[i][j], X2[i][j]) for j in range(num)] for i in range(num)])
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.plot_surface(X1, X2, Z, cmap='hsv')
-    ax.set_xlabel('X1')
-    ax.set_ylabel('X2')
-    ax.set_zlabel('f(X1, X2)')
-    ax.set_title('Branin RCos Function')
-
-    plt.show()
+    BraninsRcosFunction().print_plot_with_points([])
